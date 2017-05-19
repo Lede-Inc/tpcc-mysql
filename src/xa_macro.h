a0fb9ffcd906839ecfd9ea0c353fb46f76d72b81
weihe <hewei.chn@gmail.com>
2017-05-19 17:47:09 +0800

    add xa rate again for test.

--- src/xa_macro.h
+++ src/xa_macro.h
@@ -3,14 +3,19 @@
 
 extern int * transaction_flag;
 extern char * XA_FLAG;
+extern int XA_RATE[5];
 #define START_TRANS(t_num, table, key) \
 do {\
 if (transaction_flag[t_num] != 1) {\
+	mysql_query(ctx[t_num],"START TRANSACTION"); \
 	if(strlen(table) > 0) {\
-		mysql_query(ctx[t_num],"START TRANSACTION"); \
-		mysql_trans_no_xa(ctx[t_num], table, key);\
-	} else {\
-		mysql_query(ctx[t_num],"START TRANSACTION"); \
+		if (XA_RATE[0] == 0) {\
+			mysql_trans_no_xa(ctx[t_num], table, key);\
+		} else { \
+			if (RandomNumber(1,100) > XA_RATE[0]) { \
+				mysql_trans_no_xa(ctx[t_num], table, key);\
+			}\
+		}\
 	}\
 	transaction_flag[t_num] = 1;\
 } } while(0)
